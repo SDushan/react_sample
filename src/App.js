98207f9646bc4b6fbc03763cc05285d56a0af605
SDushan <samithadushan@gmail.com>
2022-01-26 15:28:24 +0530

    config routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+
+import Meetups from "./pages/Meetups";
+import NewMeetup from "./pages/NewMeetup";
+import Favourites from "./pages/Favourites";
+
 function App() {
-  return <div>hello!</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Meetups />} />
+        <Route path="/new-meetup" element={<NewMeetup />} />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
