2f6916401f546b4439c5537b31a368ba8c7099c5
SDushan <samithadushan@gmail.com>
2022-01-26 16:11:24 +0530

    added main navigation header component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import Meetups from "./pages/Meetups";
 import NewMeetup from "./pages/NewMeetup";
 import Favourites from "./pages/Favourites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<Meetups />} />
         <Route path="/new-meetup" element={<NewMeetup />} />
